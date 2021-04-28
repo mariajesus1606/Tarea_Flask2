dde51d857c3880ce1f9dc61b7d8f5a2f215f9cca
mariajesus1606 <61273346+mariajesus1606@users.noreply.github.com>
2021-04-28 19:10:13 +0200

    He definido la ruta de la pagina de inicio

--- app.py
+++ app.py
@@ -1,1 +1,22 @@
 
+#Importamos lo que vamos a necesitar de flask
+from flask import Flask, render_template,abort
+# Importamos la libreria os del sistema para utilizar environ
+import os 
+# La aplicación nos va permitir mostrar la información del fichero books.json
+# Por tanto, importamos json para leer el fichero 
+import json 
+# Definimos la variable app por Flask
+app = Flask(__name__)
+# Leemos el fichero y lo añadimos a una variable 
+with open ("books.json") as fichero:
+    info = json.load(fichero)
+
+# Definimos la ruta principal de la página de incio 
+
+@app.route('/',methods=["GET","POST"])
+def inicio():
+	return render_template("inicio.html", lista_libros=info)
+
+#Probar en el entorno de desarrollo
+app.run(debug=True)
