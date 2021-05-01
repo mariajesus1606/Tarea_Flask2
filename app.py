004ec1e40aeb8591fd7160dcd5235e3219f30149
mariajesus1606 <61273346+mariajesus1606@users.noreply.github.com>
2021-05-01 18:33:19 +0200

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from flask import Flask, render_template,abort
 # Importamos la libreria os del sistema para utilizar environ
 import os 
 # La aplicación nos va permitir mostrar la información del fichero books.json
-# Por tanto, importamos json para leer el fichero 
+# Importamos json para leer el fichero 
 import json 
 # Definimos la variable app por Flask
 app = Flask(__name__)
